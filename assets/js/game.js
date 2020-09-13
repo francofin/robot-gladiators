20aa2aa7290224097861a820b0779cc5dda44553
fintank <finapptank@gmail.com>
2020-09-13 16:49:44 -0400

    added console and function js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,33 @@
 // window.alert allows us to add a message to the page as the user loads the page. 
 
 window.alert("This is an alert! JavaScript is running!");
-window.alert("Please ensure this adds to github");
+
+
+// window.prompt allows us to get information from the user
+var playerName = window.prompt("What is your robot's name?");
+//window.alert("Welcome to the fight " + playerName);
+//console.log is the developers console
+console.log(playerName);
+
+//console examples
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+
+// fight function
+
+function fight() {
+    window.alert("Fight To The Death")
+}
+
+fight()
+
+// // This is a String data type; it must be wrapped in double quotes (" ") or single quotes (' ').
+// var stringDataType = "This is a string, which is a fancy way to say text";
+
+// // This is a Number data type; it can be an integer (whole number) or have decimals (floated numbers).
+// var numberIntegerDataType = 10;
+// var numberFloatDataType = 10.4;
+
+// // This is a Boolean data type, which can only be given a value of true or false.
+// var booleanDataType = true;
