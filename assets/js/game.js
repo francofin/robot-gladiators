3f7d684bbd00a092165da19a892e1fa81a3ebbca
fintank <finapptank@gmail.com>
2020-09-13 16:19:54 -0400

    added test message

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,4 @@
 // window.alert allows us to add a message to the page as the user loads the page. 
 
 window.alert("This is an alert! JavaScript is running!");
-
+window.alert("Please ensure this adds to github");
